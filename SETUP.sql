e6c85c76c44e49d72e448bc63f1f90be695b4003
klimtxoxo-hash <klimtxoxo@gmail.com>
2026-04-10 11:36:44 -0400

    Delete SETUP.sql

--- SETUP.sql
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
-);
-
-export const PRACTICE_SLUG = process.env.REACT_APP_PRACTICE_SLUG || 'demo';
